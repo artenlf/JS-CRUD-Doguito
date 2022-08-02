b07d8f090673cc28d59fdbbb46f1fa6af9a36402
artenlf <felipe.arten@gmail.com>
2022-08-02 20:51:20 -0300

    Adicionando os arquivos do projeto

---
+++ service/cliente-service.js
@@ -0,0 +1,36 @@
+// exemplo utilizando fetch ------
+
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
+            return resposta.json();
+        });
+};
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.body
+        });
+};
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE',
+    });
+};
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente
+};
